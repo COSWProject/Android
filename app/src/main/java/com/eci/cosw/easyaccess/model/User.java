bd8e4b004433ec56c0bc56cc18fb2659e077224a
Camilo López <camilino123@gmail.com>
2019-05-08 16:40:45 -0500

    User POJO created

---
+++ app/src/main/java/com/eci/cosw/easyaccess/model/User.java
@@ -0,0 +1,21 @@
+package com.eci.cosw.easyaccess.model;
+
+public class User {
+
+    private String id;
+
+    private int cardId;
+    private String name;
+    private String email;
+    private String address;
+    private String password;
+    private String organization;
+    private int mobilePhone;
+    private int homePhone;
+    private String occupation;
+    private String city;
+    private String state;
+    private int postalCode;
+    private String country;
+    private String rol;
+}
