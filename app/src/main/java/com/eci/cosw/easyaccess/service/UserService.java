a15dbc9cd45c956380817d00a717cfdcadbc2a18
Camilo López <camilino123@gmail.com>
2019-05-14 18:12:30 -0500

    Get user by email get method created

--- app/src/main/java/com/eci/cosw/easyaccess/service/UserService.java
+++ app/src/main/java/com/eci/cosw/easyaccess/service/UserService.java
@@ -5,10 +5,15 @@ import com.eci.cosw.easyaccess.model.User;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface UserService {
 
     @POST("token/newUser")
     Call<ResponseBody> createUser(@Body User user);
+
+    @GET("api/user/email/{email}")
+    Call<User> getUserByEmail(@Path("email") String email);
 }
