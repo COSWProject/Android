eaf3e9760a8f84d8fabc4d13ec939f9655c8af6d
Camilo López <camilino123@gmail.com>
2019-05-14 14:50:37 -0500

    User attributes edited

--- app/src/main/java/com/eci/cosw/easyaccess/model/User.java
+++ app/src/main/java/com/eci/cosw/easyaccess/model/User.java
@@ -2,20 +2,79 @@ package com.eci.cosw.easyaccess.model;
 
 public class User {
 
-    private String id;
-
-    private int cardId;
     private String name;
     private String email;
-    private String address;
     private String password;
-    private String organization;
     private int mobilePhone;
-    private int homePhone;
     private String occupation;
     private String city;
-    private String state;
-    private int postalCode;
-    private String country;
-    private String rol;
+
+    public User(String name, String email, String password, int mobilePhone, String occupation, String city) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.mobilePhone = mobilePhone;
+        this.occupation = occupation;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(int mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", mobilePhone=" + mobilePhone +
+                ", occupation='" + occupation + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
