a6156409f3d90f062adad01d0c7053e0c40d7e10
Camilo López <camilino123@gmail.com>
2019-05-14 18:10:15 -0500

    Name package changed

--- app/src/androidTest/java/com/eci/cosw/easyaccess/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/eci/cosw/easyaccess/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.easyaccess", appContext.getPackageName());
+        assertEquals("com.eci.cosw.easyaccess", appContext.getPackageName());
     }
 }
