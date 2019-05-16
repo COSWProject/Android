aac233bbf98b8b1c6566e06c880de7a08a064c26
Camilo López <camilino123@gmail.com>
2019-05-16 18:43:39 -0500

    Attributes changed

--- app/src/main/java/com/eci/cosw/easyaccess/model/Access.java
+++ app/src/main/java/com/eci/cosw/easyaccess/model/Access.java
@@ -2,58 +2,88 @@ package com.eci.cosw.easyaccess.model;
 
 public class Access {
 
-        private String id;
-        private String owner;
-        private String qr;
-        private String invitedBy;
-        private String Institution;
-        private String time;
-        private String date;
-        private String expirationTime;
+    private String id;
+    private String owner;
+    private String qr;
+    private String invitedBy;
+    private String institution;
+    private String time;
+    private String date;
+    private String expirationTime;
 
-        public Access(){
+    public Access() {
 
-        }
+    }
 
-        public Access( String owner, String qr, String invitedBy, String institution,
-                       String time, String date, String expirationTime) {
-            this.owner = owner;
-            this.qr = qr;
-            this.invitedBy = invitedBy;
-            Institution = institution;
-            this.time = time;
-            this.date = date;
-            this.expirationTime = expirationTime;
-        }
+    public Access(String owner, String qr, String invitedBy, String institution,
+                  String time, String date, String expirationTime) {
+        this.owner = owner;
+        this.qr = qr;
+        this.invitedBy = invitedBy;
+        this.institution = institution;
+        this.time = time;
+        this.date = date;
+        this.expirationTime = expirationTime;
+    }
 
-        public String getId() {return id;}
+    public String getId() {
+        return id;
+    }
 
-        public String getOwner() {return owner; }
+    public String getOwner() {
+        return owner;
+    }
 
-        public void setOwner(String owner) { this.owner = owner; }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 
-        public String getQr() { return qr; }
+    public String getQr() {
+        return qr;
+    }
 
-        public void setQr(String qr) { this.qr = qr; }
+    public void setQr(String qr) {
+        this.qr = qr;
+    }
 
-        public String getInvitedBy() { return invitedBy; }
+    public String getInvitedBy() {
+        return invitedBy;
+    }
 
-        public void setInvitedBy(String invitedBy) { this.invitedBy = invitedBy; }
+    public void setInvitedBy(String invitedBy) {
+        this.invitedBy = invitedBy;
+    }
 
-        public String getInstitution() { return Institution; }
+    public String getInstitution() {
+        return institution;
+    }
 
-        public void setInstitution(String institution) { Institution = institution; }
+    public void setInstitution(String institution) {
+        institution = institution;
+    }
 
-        public String getTime() { return time; }
+    public String getTime() {
+        return time;
+    }
 
-        public void setTime(String time) { this.time = time; }
+    public void setTime(String time) {
+        this.time = time;
+    }
 
-        public String getDate() { return date; }
+    public String getDate() {
+        return date;
+    }
 
-        public void setDate(String date) { this.date = date; }
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-        public String getExpirationTime() { return expirationTime; }
+    public String getExpirationTime() {
+        return expirationTime;
+    }
 
-        public void setExpirationTime(String expirationTime) { this.expirationTime = expirationTime;}
+    public void setExpirationTime(String expirationTime) {
+        this.expirationTime = expirationTime;
+    }
 
 }
