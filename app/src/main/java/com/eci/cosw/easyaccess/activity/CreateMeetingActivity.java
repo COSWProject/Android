9b7a855c3cfd750e9ae3629260d0817982b7f58b
Camilo López <camilino123@gmail.com>
2019-05-16 18:42:35 -0500

    sharedPreference and retrofitHttp instance created

--- app/src/main/java/com/eci/cosw/easyaccess/activity/CreateMeetingActivity.java
+++ app/src/main/java/com/eci/cosw/easyaccess/activity/CreateMeetingActivity.java
@@ -8,8 +8,20 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.eci.cosw.easyaccess.R;
+import com.eci.cosw.easyaccess.service.AccessService;
+import com.eci.cosw.easyaccess.service.UserService;
+import com.eci.cosw.easyaccess.util.RetrofitHttp;
+import com.eci.cosw.easyaccess.util.SharedPreference;
 
 public class CreateMeetingActivity extends AppCompatActivity {
+
+    private SharedPreference sharedPreference;
+    private RetrofitHttp retrofitHttp;
+
+    private String file;
+
+    private UserService userService;
+    private AccessService accessService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +30,19 @@ public class CreateMeetingActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        file = getString(R.string.preference_file_key);
+
+        sharedPreference = new SharedPreference(this, file);
+
+        retrofitHttp = new RetrofitHttp(sharedPreference.getValue("TOKEN_KEY"));
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = new Intent(this, MainCompanyActivity.class);
         startActivity(intent);
-        finish();
         return true;
     }
+
 }
