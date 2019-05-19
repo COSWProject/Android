d8e98a823b4490fa6114a84a0dc540314400e576
Camilo López <camilino123@gmail.com>
2019-05-19 13:49:33 -0500

    Arrow back logic implemented

--- app/src/main/java/com/eci/cosw/easyaccess/UserProfileActivity.java
+++ app/src/main/java/com/eci/cosw/easyaccess/UserProfileActivity.java
@@ -1,9 +1,13 @@
 package com.eci.cosw.easyaccess;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.eci.cosw.easyaccess.activity.MainUserActivity;
 import com.eci.cosw.easyaccess.util.RetrofitHttp;
 import com.eci.cosw.easyaccess.util.SharedPreference;
 
@@ -19,9 +23,19 @@ public class UserProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         file = getString(R.string.preference_file_key);
         sharedPreference = new SharedPreference(this, file);
 
         retrofitHttp = new RetrofitHttp(sharedPreference.getValue("TOKEN_KEY"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, MainUserActivity.class);
+        startActivity(intent);
+        return true;
     }
 }
