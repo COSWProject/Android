a95c8d2e39333c7fdf9638e8b6fb7b50893112ae
Camilo López <camilino123@gmail.com>
2019-05-19 13:46:58 -0500

    Shared preference and retrofit instances created

--- app/src/main/java/com/eci/cosw/easyaccess/UserProfileActivity.java
+++ app/src/main/java/com/eci/cosw/easyaccess/UserProfileActivity.java
@@ -1,14 +1,27 @@
 package com.eci.cosw.easyaccess;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eci.cosw.easyaccess.util.RetrofitHttp;
+import com.eci.cosw.easyaccess.util.SharedPreference;
+
 public class UserProfileActivity extends AppCompatActivity {
+
+    private RetrofitHttp retrofitHttp;
+    private SharedPreference sharedPreference;
+
+    private String file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        file = getString(R.string.preference_file_key);
+        sharedPreference = new SharedPreference(this, file);
+
+        retrofitHttp = new RetrofitHttp(sharedPreference.getValue("TOKEN_KEY"));
     }
 }
