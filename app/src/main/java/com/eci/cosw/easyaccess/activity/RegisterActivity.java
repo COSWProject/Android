1f101111007ee911ecbcf8241948c3d2449e1d86
Camilo López <camilino123@gmail.com>
2019-05-20 11:59:21 -0500

    Get num Id and full name from Cedula done

--- app/src/main/java/com/eci/cosw/easyaccess/activity/RegisterActivity.java
+++ app/src/main/java/com/eci/cosw/easyaccess/activity/RegisterActivity.java
@@ -3,12 +3,15 @@ package com.eci.cosw.easyaccess.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,12 +19,22 @@ import com.eci.cosw.easyaccess.R;
 import com.eci.cosw.easyaccess.model.User;
 import com.eci.cosw.easyaccess.service.UserService;
 import com.eci.cosw.easyaccess.util.RetrofitHttp;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -31,6 +44,7 @@ import retrofit2.Response;
 public class RegisterActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 1;
+    private static final String TAG = "";
     private RetrofitHttp retrofitHttp;
     private UserService userService;
 
@@ -44,6 +58,8 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText userCity;
     private EditText userCedula;
 
+    private List<String> userInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +67,8 @@ public class RegisterActivity extends AppCompatActivity {
 
         retrofitHttp = new RetrofitHttp();
         userService = retrofitHttp.getRetrofit().create(UserService.class);
+
+        userInfo = new ArrayList<String>();
     }
 
     public void findViews() {
@@ -141,9 +159,78 @@ public class RegisterActivity extends AppCompatActivity {
                 InputStream imageStream = getContentResolver().openInputStream(imageUri);
                 final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
 
-                FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(selectedImage);
+                FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage
+                        .fromBitmap(selectedImage);
+
+                FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = FirebaseVision
+                        .getInstance().getOnDeviceTextRecognizer();
+
+                Task<FirebaseVisionText> firebaseVisionTextTask =
+                        firebaseVisionTextRecognizer.processImage(firebaseVisionImage)
+                                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                                    @Override
+                                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                        obtainText(firebaseVisionText);
+
+                                        obtainAllUserInfoFromCedula();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                    }
+                                });
+
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    private void obtainText(FirebaseVisionText firebaseVisionText) {
+        String resultText = firebaseVisionText.getText();
+        for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
+            String blockText = block.getText();
+            Float blockConfidence = block.getConfidence();
+            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+            Point[] blockCornerPoints = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (FirebaseVisionText.Line line : block.getLines()) {
+                String lineText = line.getText();
+                userInfo.add(lineText);
+                Float lineConfidence = line.getConfidence();
+                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (FirebaseVisionText.Element element : line.getElements()) {
+                    String elementText = element.getText();
+                    Float elementConfidence = element.getConfidence();
+                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                }
+            }
+        }
+    }
+
+    private void obtainAllUserInfoFromCedula() {
+        List<Character> strings = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+        String fullName = "";
+        String fullId = "";
+        for (String x : userInfo) {
+            if (!(x.contains("NOMBRES") || x.contains("REPUBLICA") ||
+                    x.contains("CEDULA") || x.contains("APELLIDOS") ||
+                    x.contains("IDENTIFICACION") || x.contains("FIRMA"))) {
+                if (x.contains("NUMERO")) {
+                    for (char y : x.toCharArray()) {
+                        if (strings.contains(y)) {
+                            fullId += y;
+                        }
+                    }
+                } else {
+                    fullName += x;
+                }
             }
         }
     }
